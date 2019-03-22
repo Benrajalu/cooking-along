bbfc5433ad48c3e041f8d7b820dbb5abbcfff67b
Benoit R <rajalubenoit@gmail.com>
2019-03-22 20:44:45 +0100

    misc(Setup): CRA + TS + Storybook

---
+++ src/components/test/Shell.stories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Shell from "./Shell";
+
+storiesOf("Shell", module).add("Default", () => <Shell />);
---
+++ src/components/test/Shell.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Shell extends Component {
+  render() {
+    return <div className="Shell">Hello World</div>;
+  }
+}
+
+export default Shell;
