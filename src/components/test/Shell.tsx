abb949e38bc9b9367e12a7bedc6653aac8cbaa5a
Benoit R <rajalubenoit@gmail.com>
2019-03-24 21:53:02 +0100

    misc(Setup): FontIcon setup

--- src/components/test/Shell.tsx
+++ src/components/test/Shell.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Shell.module.scss";
 
 class Shell extends Component {
   render() {
-    return <div className={styles.shell}>Hello World</div>;
+    return (
+      <div className={styles.shell}>
+        <FontAwesomeIcon icon={faCoffee} />
+        Hello World
+      </div>
+    );
   }
 }
 
